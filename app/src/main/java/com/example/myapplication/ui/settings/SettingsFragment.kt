c7620fc5e1887db1feff8a71a455f46f9c8d0163
Lenovo <paluszekwskazujacy@gmail.com>
2020-10-20 12:26:15 +0200

    navigation bottom set

---
+++ app/src/main/java/com/example/myapplication/ui/settings/SettingsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.ui.settings
+
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : Fragment() {
+
+}
